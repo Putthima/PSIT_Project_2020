38ac9ecb14f7c68f53d1e4b1ccc9ac6a0f394be4
Putthima Dechdumnil <73655899+Putthima@users.noreply.github.com>
2020-12-12 16:12:06 +0700

    [Update 100%]  Combo box เลือกปีและเดือน

--- Option for calendar.py
+++ Option for calendar.py
@@ -17,13 +17,13 @@ def on_click(e):
 
 import tkinter as tk
 # frames = Frame(root)
-back_but = Button(root, text="Back", bg='purple1')
+back_but = Button(root, text="Back", bg='purple1', width=5, padx=3)
 back_but.grid(row=1, column=2)
-next_but = Button(root, text="Next", bg='gold')
+next_but = Button(root, text="Next", bg='gold', width=5, padx=3)
 next_but.grid(row=1, column=3)
 
-btn = Button(root, text="Search", bg='salmon')
-btn.grid(row=2, column=2, columnspan=3)
+btn = Button(root, text="Search", bg='salmon', width=11)
+btn.grid(row=2, column=2, columnspan=4)
 btn.bind('<Button-1>', on_click)
 
 root.mainloop()
